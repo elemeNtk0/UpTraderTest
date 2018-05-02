aa9b923a4c8a4d205109a70897b9a215ca420e02
a.senchenko <a.senchenko@space307.com>
2018-05-03 02:04:04 +0300

    чёт какая-то дичь

--- docs/dist/js/myplugin.js
+++ docs/dist/js/myplugin.js
@@ -5,4 +5,26 @@
  * 
  */
 
-// console.log("Ебать колотить!");
+document.addEventListener('DOMContentLoaded', function() {
+  function mediaQuery(x) {
+    if (x.matches) {
+      desktopAccordion();
+    } else {
+      mobileAccordion();
+    }
+  }
+  
+  var x = window.matchMedia("(min-width: 480px)");
+  mediaQuery(x);
+  x.addListener(mediaQuery);
+
+
+  function mobileAccordion() {
+    console.log("МОБАЙЛ ФЁРСТ, СУКА!");
+  }
+
+  function desktopAccordion() {
+    console.log("Ну вот и декстопчик > 480");
+  }
+  
+});
--- src/js/myplugin.js
+++ src/js/myplugin.js
@@ -1,1 +1,23 @@
-// console.log("Ебать колотить!");
+document.addEventListener('DOMContentLoaded', function() {
+  function mediaQuery(x) {
+    if (x.matches) {
+      desktopAccordion();
+    } else {
+      mobileAccordion();
+    }
+  }
+  
+  var x = window.matchMedia("(min-width: 480px)");
+  mediaQuery(x);
+  x.addListener(mediaQuery);
+
+
+  function mobileAccordion() {
+    console.log("МОБАЙЛ ФЁРСТ, СУКА!");
+  }
+
+  function desktopAccordion() {
+    console.log("Ну вот и декстопчик > 480");
+  }
+  
+});
